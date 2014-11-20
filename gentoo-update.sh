83a19a2a2194334c2e4340679070ee8f67186336
Mark Pariente <markpariente@gmail.com>
2014-11-20 11:18:15 -0800

    gentoo-update: allow preserved rebuild exclude

--- gentoo-update.sh
+++ gentoo-update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+PREXCLUDEPATH="/etc/portage/preserved-rebuild.exclude"
+
 if [ "$1" != "--no-sync" ] ; then
    layman -S || { echo "Layman failed"; exit 1;}
    eix-sync || { echo "eix-sync failed"; exit 1;}
@@ -15,5 +18,13 @@ fi
 
 ${DISTCC_CMD} emerge -avuDN --jobs --load-average 9 --with-bdeps=y @world || { echo "emerge failed"; exit 1;}
 emerge --ask --depclean || { echo "depclean failed"; exit 1;}
-${DISTCC_CMD} emerge -a1v --jobs --load-average 9 @preserved-rebuild || { echo "preserved-rebuild failed"; exit 1;}
+
+if [ -e "$PREXCLUDEPATH" ]
+then
+   PREXCLUDE="--exclude `cat \"$PREXCLUDEPATH\"`"
+else
+   PREXCLUDE=""
+fi
+
+${DISTCC_CMD} emerge -a1v --jobs --load-average 9 $PREXCLUDE @preserved-rebuild || { echo "preserved-rebuild failed"; exit 1;}
 ionice -c3 revdep-rebuild -i -- --jobs --load-average 9 || { echo "revdep-rebuild failed"; exit 1;}
